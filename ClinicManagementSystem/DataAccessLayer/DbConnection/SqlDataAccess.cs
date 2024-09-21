2edfe99a9d76ff1329b7a9697bd8ca29e6c064c4
Orlando Zúniga <139894785+orlando-zh@users.noreply.github.com>
2024-09-21 13:14:05 -0600

    subiendo ejercicios clases

---
+++ ClinicManagementSystem/DataAccessLayer/DbConnection/SqlDataAccess.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.DbConnection
+{
+    public class SqlDataAccess
+    {
+        private readonly string _connectionString;
+
+        public SqlDataAccess()
+        {
+            _connectionString = "Data Source=LAPTOP-KFDQNCRJ\\SQLEXPRESS;Initial Catalog=ClinicDB;Integrated Security=True;Encrypt=False";
+        }
+
+        public SqlConnection GetConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
+    }
+}
---
+++ ClinicManagementSystem/DataAccessLayer/Repositories/DoctorRepository.cs
@@ -0,0 +1,39 @@
+﻿using DataAccessLayer.DbConnection;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class DoctorRepository
+    {
+        private SqlDataAccess _dbConnection;
+
+        public DoctorRepository()
+        {
+            _dbConnection = new SqlDataAccess();
+        }
+
+        public DataTable GetDoctors()
+        {
+            DataTable doctorsTable = new DataTable();
+
+            using (var connection = _dbConnection.GetConnection())
+            {
+                string query = "SELECT Id, FirstName, LastName FROM Doctor";
+
+                SqlCommand command = new SqlCommand(query, connection);
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+                doctorsTable.Load(reader);
+            }
+
+            return doctorsTable;
+        }
+    }
+}
